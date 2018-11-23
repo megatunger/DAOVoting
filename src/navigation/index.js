e8177864ea70a127cbc397e20a432144f9e0d7a5
Lam Ha Thai <thailh12@gmail.com>
2018-11-24 02:03:59 +0700

    fix navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,11 +1,22 @@
-import { createBottomTabNavigator } from 'react-navigation';
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from 'react-navigation';
 import { Home } from '../components/Home';
 import { Counter } from '../components/Counter';
 import { LoginScreen, LoginScreenView } from '../components/LoginScreen';
 
-
-export const Nav = createBottomTabNavigator({
-  LoginScreen: LoginScreenView,
+export const auth = createStackNavigator({ LoginScreen: LoginScreenView });
+export const app = createBottomTabNavigator({
   Home: Home,
   Ahihihisdfd: Counter
 });
+export const Nav = createStackNavigator(
+  {
+    auth: { screen: LoginScreenView, navigationOptions: { header: null } },
+    App: { screen: app, navigationOptions: { header: null } }
+  },
+  {
+    initialRouteName: 'auth'
+  }
+);
