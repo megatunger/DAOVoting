123200df3b08d1704a579357bc35214d8f28cb33
Lam Ha Thai <thailh12@gmail.com>
2018-11-24 17:41:28 +0700

    styled card

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,8 @@ import {
   View,
   Text,
   StyleSheet,
-  Button
+  Button,
+  ScrollView
 } from 'react-native';
 import CounterContainer from '../containers/CounterContainer';
 import { Card } from '../elements/Card';
@@ -25,51 +26,66 @@ class HomeView extends React.Component {
   };
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <StatusBar hidden={true} />
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'row',
-            marginTop: 30,
-            marginBottom: 30,
-            marginLeft: 30,
-            marginRight: 30,
-            justifyContent: 'space-between'
-          }}
-        >
-          <Icon
-            onPress={() => this.showActionSheet()}
-            name="bars"
-            color="#FFFFFF"
-            size={30}
-          />
-          <Image
-            style={{ width: 190 / 1.8, height: 66 / 1.8 }}
-            source={require('../images/logo3.png')}
-          />
-          <Avatar
-            medium
-            rounded
-            source={{
-              uri:
-                'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={{ justifyContent: 'center' }}
+      >
+        <SafeAreaView>
+          <ActionSheet
+            style={{ height: 1 }}
+            ref={o => (this.ActionSheet = o)}
+            title={'Which one do you like ?'}
+            options={['Apple', 'Banana', 'cancel']}
+            cancelButtonIndex={2}
+            destructiveButtonIndex={1}
+            onPress={index => {
+              /* do something */
             }}
-            onPress={() => console.log('Works!')}
-            activeOpacity={0.7}
           />
-        </View>
-        <ActionSheet
-          ref={o => (this.ActionSheet = o)}
-          title={'Which one do you like ?'}
-          options={['Apple', 'Banana', 'cancel']}
-          cancelButtonIndex={2}
-          destructiveButtonIndex={1}
-          onPress={index => {
-            /* do something */
-          }}
-        />
-      </SafeAreaView>
+          <StatusBar hidden={true} />
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              marginTop: 30,
+              marginLeft: 30,
+              marginRight: 30,
+              marginBottom: 30,
+              justifyContent: 'space-between'
+            }}
+          >
+            <View style={{ height: 30 }}>
+              <Icon
+                onPress={() => this.showActionSheet()}
+                name="bars"
+                color="#FFFFFF"
+                size={30}
+              />
+            </View>
+
+            <Image
+              style={{ width: 190 / 1.8, height: 66 / 1.8 }}
+              source={require('../images/logo3.png')}
+            />
+            <Avatar
+              medium
+              rounded
+              source={{
+                uri:
+                  'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'
+              }}
+              onPress={() => console.log('Works!')}
+              activeOpacity={0.7}
+            />
+          </View>
+          <View style={{ flex: 1, alignItems: 'center' }}>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </View>
+        </SafeAreaView>
+      </ScrollView>
     );
   }
 }
@@ -90,16 +106,6 @@ export class Home extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     backgroundColor: '#392E42'
-  },
-  input: {
-    width: 300,
-    height: 45,
-    padding: 10,
-    borderWidth: 1,
-    marginBottom: 20,
-    backgroundColor: 'rgba(197,197,197,0.35)',
-    borderRadius: 5
   }
 });
--- src/elements/Card.js
+++ src/elements/Card.js
@@ -1,11 +1,57 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 import { Title } from './Title';
+import LinearGradient from 'react-native-linear-gradient';
 
 export const Card = props => {
   return (
-    <View>
-      <Title>{props.name}</Title>
-    </View>
+    <LinearGradient
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+      colors={['#00c091', '#04795c']}
+      style={styles.container}
+    >
+      <View style={styles.left}>
+        <Image
+          style={{ width: '100%', height: '100%', padding: 20 }}
+          source={require('../images/logo3.png')}
+        />
+        <View
+          style={{
+            position: 'absolute',
+            margin: 100,
+            backgroundColor: '#000',
+            width: 120,
+            height: 30
+          }}
+        >
+          <Text style={{ color: '#ffff', textAlign: 'center' }}>
+            2 days left
+          </Text>
+        </View>
+      </View>
+      <View style={styles.right}>
+        <Text style={{ fontSize: 17, color: '#ffff' }}>title</Text>
+        <Text style={{ color: '#ffff' }}>
+          aslkdfjwpoienvksnvasnvkadsn;foajwe;i
+        </Text>
+        <Text style={{ fontSize: 13, color: '#ffff', bottom: -40 }}>votes</Text>
+      </View>
+    </LinearGradient>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 5,
+    width: Dimensions.get('window').width - 20,
+    height: 150,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 20,
+    borderRadius: 5
+  },
+  left: { width: '50%', alignItems: 'center', padding: 10 },
+  right: { width: '50%', alignItems: 'center', padding: 10 }
+});
