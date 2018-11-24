7250c16ca76eff37ed8a384e798d4920a891edd2
Lam Ha Thai <thailh12@gmail.com>
2018-11-24 14:33:48 +0700

    tested actionsheet

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,74 @@
 import React from 'react';
-import {Avatar} from 'react-native-elements';
-import {Image, StatusBar, SafeAreaView, View, Text, StyleSheet,Button } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import {
+  Image,
+  StatusBar,
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  Button
+} from 'react-native';
 import CounterContainer from '../containers/CounterContainer';
 import { Card } from '../elements/Card';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import HomeContainer from '../containers/HomeContainer';
 import { Subscribe } from 'unstated';
+import ActionSheet from 'react-native-actionsheet';
 
 /**
  * Build up screen
  */
 class HomeView extends React.Component {
+  showActionSheet = () => {
+    this.ActionSheet.show();
+  };
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <StatusBar hidden={true} />
-        <View style={{flex: 1, flexDirection: 'row', marginTop:30, marginBottom: 30, marginLeft:30, marginRight:30, justifyContent: 'space-between'}}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            marginTop: 30,
+            marginBottom: 30,
+            marginLeft: 30,
+            marginRight: 30,
+            justifyContent: 'space-between'
+          }}
+        >
           <Icon
-                    name="bars"
-                    color='#FFFFFF'
-                    size={30}
-                />
-          <Image
-          style={{ width: 190/1.8, height: 66/1.8 }}
-          source={require('../images/logo3.png')}
+            onPress={() => this.showActionSheet()}
+            name="bars"
+            color="#FFFFFF"
+            size={30}
           />
-          <Avatar  
+          <Image
+            style={{ width: 190 / 1.8, height: 66 / 1.8 }}
+            source={require('../images/logo3.png')}
+          />
+          <Avatar
             medium
             rounded
-            source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"}}
-            onPress={() => console.log("Works!")}
+            source={{
+              uri:
+                'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'
+            }}
+            onPress={() => console.log('Works!')}
             activeOpacity={0.7}
           />
         </View>
+        <ActionSheet
+          ref={o => (this.ActionSheet = o)}
+          title={'Which one do you like ?'}
+          options={['Apple', 'Banana', 'cancel']}
+          cancelButtonIndex={2}
+          destructiveButtonIndex={1}
+          onPress={index => {
+            /* do something */
+          }}
+        />
       </SafeAreaView>
     );
   }
@@ -50,8 +86,6 @@ export class Home extends React.Component {
     );
   }
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
